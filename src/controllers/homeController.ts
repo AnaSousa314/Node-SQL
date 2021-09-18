0db7eef3c196d46fc82d1780016952071fd6828e
Ana.Sousa <anasousa314@outlook.com>
2021-09-18 16:38:03 -0300

    aula22 tipos de consulta 2

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
+import {Op} from "sequelize"
 
 import { Product } from '../models/Product';
 import { User } from "../models/Users";
 
-import {Op} from "sequelize"
 
 export const home = async (req: Request, res: Response)=>{
 
+    let searchName:string = 'an' 
     // let users = await User.findAll();
     // console.log("Usuários: ",JSON.stringify(users))
     
@@ -17,16 +18,37 @@ export const home = async (req: Request, res: Response)=>{
         //     name: 'Paulo',
         //     age: 30,
         // }
+        // where:{
+        //     // age:{
+        //     //     [Op.or]: [30,55]
+        //     // }, essa forma é mais indicada p coisas mais complexas, a abaixo é mais facil de aprender
+        //     // [Op.or]: [
+        //     //     {age: 55},
+        //     //     {age: 26},
+        //     //     {name: 'Paulo'},
+        //     // ]
+        //     age: [55,30,18]//busca todos q tiverem essas condicoes 
+        // }
+
         where:{
             // age:{
-            //     [Op.or]: [30,55]
-            // }, essa forma é mais indicada p coisas mais complexas, a abaixo é mais facil de aprender
-            // [Op.or]: [
-            //     {age: 55},
-            //     {age: 26},
-            //     {name: 'Paulo'},
-            // ]
-            age: [55,30,18]//busca todos q tiverem essas condicoes 
+        //     // GT - Greater Than,
+        //     // E - Equal,
+        //     // LT - Lower Than
+        //     // [Op.gt]: 40, // > 40,
+        //     // [Op.gte]: 30, // >= 40
+        //     // [Op.lt]: 40, // < 40
+        //     // [Op.lte]: 55, // <= 40
+        //     // [Op.between]: [30,55]
+        //     // [Op.notBetween]: [30,55]
+        //    // [Op.in]: [30,55] é o mesmo q age:[30,55]
+            //     // [Op.notIn]: [30,55]
+            // },
+            name:{
+                // [Op.like]: 'pa%'
+                // [Op.like]: '%co'
+                [Op.like]: `%${searchName}%`
+            }
         }
     });
 
