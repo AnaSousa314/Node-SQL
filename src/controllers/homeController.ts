ed26f95bbb71b6d6d5a9910503fbb5c3e0faa92a
Ana.Sousa <anasousa314@outlook.com>
2021-09-18 16:21:56 -0300

    aula21 tipos de consulta 1

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from 'express';
 
 import { Product } from '../models/Product';
-import { User } from "../models/Users"
+import { User } from "../models/Users";
+
+import {Op} from "sequelize"
 
 export const home = async (req: Request, res: Response)=>{
 
-    let users = await User.findAll();
+    // let users = await User.findAll();
     // console.log("Usuários: ",JSON.stringify(users))
-
     
+    let users = await User.findAll({
+        // attributes: ['name','age']
+        // attributes: {exclude: ['id','age']}
+        // where: {
+        //     name: 'Paulo',
+        //     age: 30,
+        // }
+        where:{
+            // age:{
+            //     [Op.or]: [30,55]
+            // }, essa forma é mais indicada p coisas mais complexas, a abaixo é mais facil de aprender
+            // [Op.or]: [
+            //     {age: 55},
+            //     {age: 26},
+            //     {name: 'Paulo'},
+            // ]
+            age: [55,30,18]//busca todos q tiverem essas condicoes 
+        }
+    });
+
+
 
 
 
