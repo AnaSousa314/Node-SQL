3459b886ebad2c7f50059fce68ddf2e57e74766a
Ana.Sousa <anasousa314@outlook.com>
2021-09-21 16:39:04 -0300

    aula23 Ordenando resultados

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -7,54 +7,64 @@ import { User } from "../models/Users";
 
 export const home = async (req: Request, res: Response)=>{
 
-    let searchName:string = 'an' 
-    // let users = await User.findAll();
-    // console.log("Usuários: ",JSON.stringify(users))
+    // let searchName:string = 'an' 
+    // // let users = await User.findAll();
+    // // console.log("Usuários: ",JSON.stringify(users))
     
+    // let users = await User.findAll({
+    //     // attributes: ['name','age']
+    //     // attributes: {exclude: ['id','age']}
+    //     // where: {
+    //     //     name: 'Paulo',
+    //     //     age: 30,
+    //     // }
+    //     // where:{
+    //     //     // age:{
+    //     //     //     [Op.or]: [30,55]
+    //     //     // }, essa forma é mais indicada p coisas mais complexas, a abaixo é mais facil de aprender
+    //     //     // [Op.or]: [
+    //     //     //     {age: 55},
+    //     //     //     {age: 26},
+    //     //     //     {name: 'Paulo'},
+    //     //     // ]
+    //     //     age: [55,30,18]//busca todos q tiverem essas condicoes 
+    //     // }
+
+    //     where:{
+    //         // age:{
+    //     //     // GT - Greater Than,
+    //     //     // E - Equal,
+    //     //     // LT - Lower Than
+    //     //     // [Op.gt]: 40, // > 40,
+    //     //     // [Op.gte]: 30, // >= 40
+    //     //     // [Op.lt]: 40, // < 40
+    //     //     // [Op.lte]: 55, // <= 40
+    //     //     // [Op.between]: [30,55]
+    //     //     // [Op.notBetween]: [30,55]
+    //     //    // [Op.in]: [30,55] é o mesmo q age:[30,55]
+    //         //     // [Op.notIn]: [30,55]
+    //         // },
+    //         name:{
+    //             // [Op.like]: 'pa%'
+    //             // [Op.like]: '%co'
+    //             [Op.like]: `%${searchName}%`
+    //         }
+    //     }
+    // });
+
+
     let users = await User.findAll({
-        // attributes: ['name','age']
-        // attributes: {exclude: ['id','age']}
-        // where: {
-        //     name: 'Paulo',
-        //     age: 30,
-        // }
-        // where:{
-        //     // age:{
-        //     //     [Op.or]: [30,55]
-        //     // }, essa forma é mais indicada p coisas mais complexas, a abaixo é mais facil de aprender
-        //     // [Op.or]: [
-        //     //     {age: 55},
-        //     //     {age: 26},
-        //     //     {name: 'Paulo'},
-        //     // ]
-        //     age: [55,30,18]//busca todos q tiverem essas condicoes 
-        // }
-
         where:{
-            // age:{
-        //     // GT - Greater Than,
-        //     // E - Equal,
-        //     // LT - Lower Than
-        //     // [Op.gt]: 40, // > 40,
-        //     // [Op.gte]: 30, // >= 40
-        //     // [Op.lt]: 40, // < 40
-        //     // [Op.lte]: 55, // <= 40
-        //     // [Op.between]: [30,55]
-        //     // [Op.notBetween]: [30,55]
-        //    // [Op.in]: [30,55] é o mesmo q age:[30,55]
-            //     // [Op.notIn]: [30,55]
-            // },
-            name:{
-                // [Op.like]: 'pa%'
-                // [Op.like]: '%co'
-                [Op.like]: `%${searchName}%`
-            }
-        }
-    });
+            age: {[Op.gte]: 18}
+        },
+        order: [
+            ['age','ASC'],
+            ['name','DESC'],
+        ]
+    })
 
 
-
-
+    //aula 23 ordenando resultados
 
     let age: number = 90;
     let showOld: boolean = false;
