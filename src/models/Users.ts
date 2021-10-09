4ced56112652a96e04e4dbdcfa8ecad7b76ac1d2
Ana.Sousa <anasousa314@outlook.com>
2021-10-09 17:14:01 -0300

    aula32 metodos getter/setter

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -14,12 +14,31 @@ export const User = sequelize.define<UserInstance>("User",{
     autoIncrement: true//assim quano usamos o findOrCreate, o sequelize entende o que está acontecendo e não dá erro
   },
   name:{
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    //antes do model entregar o nome do usuario, ele altera com o get. Ele não altera o BD, ele altera o dado entregado
+    get(){
+      const raw = this.getDataValue('name');
+      return raw.toUpperCase();
+    }
   },
   age:{
     type: DataTypes.NUMBER,
-    defaultValue: 18
-  }
+    defaultValue: 18,
+
+    // o set altera o dado antes dele ser enviado para o BD. No caso abaixo se o age que é o value, for menor que 18, ele alterara o dado de age para 18.
+    set(value:number){
+      if(value < 18){
+        value = 18;
+      }
+      this.setDataValue('age',value);
+    }
+  },
+  //com o set podemos enviar a senha já criptografada
+  // password:{
+  //   type: DataTypes.STRING
+  // }
+
+
 },{
   tableName: "users",
   timestamps: false
